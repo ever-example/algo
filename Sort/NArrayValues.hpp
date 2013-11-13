f6193e110eac3bd14d9d44e2ec86f27cbde0a551
everdistant <everdistant@gmail.com>
2013-11-12 23:16:12 -0500

    added sorting section

---
+++ Sort/NArrayValues.hpp
@@ -0,0 +1,7 @@
+#ifndef NARRAYVALUES
+#define NARRAYVALUES
+
+int values[] = {40, 10, 100, 90, 20, 25};
+int length = 6;
+
+#endif
---
+++ Sort/QuickSort/qs_simple.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../NArrayValues.hpp"
+
+using namespace std;
+
+int main() {
+	for (int n=0; 
+}
+
