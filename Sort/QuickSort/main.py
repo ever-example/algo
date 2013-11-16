537028418b08b0ab1f7258a38eb96ed2362ba909
everdistant <everdistant@gmail.com>
2013-11-16 02:38:59 -0500

    finished qs_ip.py

--- Sort/QuickSort/main.py
+++ Sort/QuickSort/main.py
@@ -6,11 +6,21 @@ from Values import *
 from qs_simple import *
 from qs_ip import *
 
+print
 print "unsorted: " 
 print values
+print
 
 print "running QuickSort(simple): " 
 print qs_simple(values[:])
+print
+
+print "running Partition once at index: " + str(length/2) + \
+		" (value of " + str(values[length/2]) + "): "
+print partition(values[:],0, length-1, length/2)[1];
+print
 
 print "running QuickSort(in place): "
 print qs_ip(values[:], 0, length-1)
+print
+
--- Sort/QuickSort/qs_ip.py
+++ Sort/QuickSort/qs_ip.py
@@ -1,13 +1,17 @@
 
 def partition(array, left, right, pivotIndex):
 	pivotValue = array[pivotIndex]
-	## move the pivot value to the end
+	## swap the pivot value to the end
 	tempValue = array[right]
 	array[pivotIndex] = tempValue
 	array[right] = pivotValue
 
 	storeIndex = left
 	
+	## at the very end, the value inside storeIndex will get swapped with
+	## the pivot value. This loop will perform partitioning with
+	## respect to the position of storeIndex, and with respect to the value
+	## of pivotValue
 	for x in range(left, right):
 		if array[x] <= pivotValue:
 			tempValue = array[x]
@@ -19,13 +23,17 @@ def partition(array, left, right, pivotIndex):
 	array[storeIndex] = array[right]
 	array[right] = tempValue
 
-	return storeIndex
+	return storeIndex, array
 
 def qs_ip(array, left, right):
 	if (left < right):
+		# select a pivot index
 		pivotIndex = left + (right-left)/2
-		pivotIndex = partition(array, left, right, pivotIndex)
-
+		# paritition with respect to the pivot
+		pivotIndex = partition(array, left, right, pivotIndex)[0]
+		# recursively sort elemnts smaller than pivot
 		qs_ip(array, left, pivotIndex-1)
+		# recursively sort elements greater than pivot
 		qs_ip(array, pivotIndex+1, right)
+		return array
 
